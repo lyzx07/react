8e9004529477fbc152d71306a7b6c05101eafb14
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-01-03 16:44:34 -0800

    circle photos

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import Employee from "./components/Employee";
 function App() {
   const showEmployees = true;
   return (
-    <div className="App bg-red-300">
+    <div className="App">
       {showEmployees ? (
         <>
-          <Employee name="caleb" role="intern"/>
-          <Employee name="abby"/>
-          <Employee name="Lindsey"/>
+          <div className="flex flex-wrap justify-center">
+            <Employee name="Caleb" role="intern" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Abby" role="dev" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Lindsey" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Caleb" role="intern" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Abby" role="dev" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Lindsey" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Caleb" role="intern" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Abby" role="dev" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+            <Employee name="Lindsey" img="https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg"/>
+          </div>
         </>
       ) : (
         <p>you can not see the employees</p>
