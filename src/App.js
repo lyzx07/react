29dec5de3384769414ee201cb4928d82700014cf
lyzx07 <86989628+lyzx07@users.noreply.github.com>
2023-01-31 22:30:38 -0800

    finishing modal changes

--- src/App.js
+++ src/App.js
@@ -7,36 +7,54 @@ function App() {
   const [role, setRole] = useState("dev");
   const [employees, setEmployees] = useState([
     {
+      id: 1,
       name: "Caleb",
       role: "Developer",
       img: "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg",
     },
     {
+      id: 2,
       name: "Sally",
       role: "Manager",
       img: "https://images.pexels.com/photos/3586798/pexels-photo-3586798.jpeg",
     },
     {
+      id: 3,
       name: "Christopher",
       role: "Intern",
       img: "https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg",
     },
     {
+      id: 4,
       name: "Wanda",
       role: "Wannabe",
       img: "https://images.pexels.com/photos/1065084/pexels-photo-1065084.jpeg",
     },
     {
+      id: 5,
       name: "Bobby",
       role: "Ceo",
       img: "https://images.pexels.com/photos/1139743/pexels-photo-1139743.jpeg",
     },
     {
+      id: 6,
       name: "Corey",
       role: "New Hire",
       img: "https://images.pexels.com/photos/2380795/pexels-photo-2380795.jpeg",
     },
   ]);
+
+  function updateEmployee(id, newName, newRole) {
+    const updatedEmployee = employees.map((employee) => {
+      if (id == employee.id) {
+        return { ...employee, name: newName, role: newRole };
+      }
+
+      return employee;
+    });
+    setEmployees(updatedEmployee);
+  }
+
   const showEmployees = true;
   return (
     <div className="App">
@@ -45,7 +63,6 @@ function App() {
           <input
             type="text"
             onChange={(e) => {
-              console.log(e.target.value);
               setRole(e.target.value);
             }}
           />
@@ -53,10 +70,12 @@ function App() {
             {employees.map((employee) => {
               return (
                 <Employee
-                  key={uuidv4()}
+                  key={employee.id}
+                  id={employee.id}
                   name={employee.name}
                   role={employee.role}
                   img={employee.img}
+                  updateEmployee={updateEmployee}
                 />
               );
             })}
